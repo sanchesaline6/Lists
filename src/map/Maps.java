5befdfe780ad5066a7c7ef01df9280db692c49ce
sanchesaline6 <sanches.aline6@gmail.com>
2021-06-06 09:07:58 -0300

    Introduz conceitos de Maps (HashMap, LinkedHashMap, TreeMap)

---
+++ src/map/Maps.java
@@ -0,0 +1,13 @@
+package map;
+
+public class Maps {
+    /*
+    * java.util.Map
+    *  - Elementos únicos (key) para cada valor (value)
+    *  - HashMap itera os elementos aleatoriamente
+    *  - LinkedHashMap itera de acordo com a ordem de inserção ou ordem de acesso
+    *  - TreeMap itera de acordo com a ordem natural das chaves ou comparator
+    *  - HashMap permite chaves nulas
+    *  - LinkedHashMap permite chaves nulas
+    *  - TreeMap não permite chaves nulas*/
+}
